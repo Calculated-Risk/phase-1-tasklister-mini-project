ef8852799b1f169a7ee417d3f66aef3da53156da
Edwin Soto <125782999+Calculated-Risk@users.noreply.github.com>
2023-06-12 21:54:35 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  let form = document.getElementById('create-task-form')
+    form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let words = document.getElementById('new-task-description')
+    console.log(words.value)
+    toDoList(words.value)
+  }) 
 });
+
+function toDoList(todo){
+let newPara = document.createElement('p')
+newPara.textContent = todo;
+document.querySelector('#tasks').appendChild(newPara)
+
+}
